b059c85f5066c2ee867af275f798bb771050a2b8
hejong2-byte <hejong2-byte@users.noreply.github.com>
2026-08-18 17:42:07 +0900

    feat: add iOS application entry point

---
+++ App/Application/SimpleCameraAutoSenderApp.swift
@@ -0,0 +1,14 @@
+import SwiftUI
+
+enum AppIdentity {
+    static let bundleIdentifier = "com.hejong2byte.simplecameraautosender"
+}
+
+@main
+struct SimpleCameraAutoSenderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Text("SimpleCamera 업무사진 전송")
+        }
+    }
+}
